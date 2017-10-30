dfbe0508ebe7065ceac222f850470d4efd671242
cmoroz1 <cmoroz1@tulane.edu>
2017-10-30 18:51:56 -0500

    Started training the model

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import numbers
+import time
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
 
 data = pd.read_csv('voice.csv')
+
+# Converts 'male' -> 1 and 'female' -> -1
+data = data.replace(['male','female'],[1,-1])
 X = data.values[:,0:-1]
 Y = data.values[:,-1]
 
@@ -14,16 +19,6 @@ for i in range(X.shape[0]):
     for j in range(X.shape[1]):
         if(not isinstance(X[i,j], numbers.Number)):
             print("NAN @ %d %d" % (i,j))
-
-# Converts 'male' -> 1 and 'female' -> -1
-for i in range(Y.shape[0]):
-    if(not isinstance(Y[i], numbers.Number)):
-        if(Y[i] == 'male'):
-            Y[i] = 1
-        elif(Y[i] == 'female'):
-            Y[i] = -1
-        else:
-            print('not found')
 
 # Splits the data into a training set and testing set
 # If not specified, testing is 25% of all the data
@@ -35,3 +30,16 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=0)
 scaler = StandardScaler().fit(X_train)
 norm_X_train = scaler.transform(X_train)
 norm_X_test = scaler.transform(X_test)
+
+# Start timer to see how long it takes to train
+t0 = time.time()
+
+# Create MLP Classifier object, with set max_iter and random_state
+mlp = MLPClassifier(max_iter=2000, random_state=0)
+mlp.fit(norm_X_train,Y_train)
+t1 = time.time()
+
+# Visualize the accuracy of the model
+print("Took %.3f sec to train model" % (t1-t0))
+print("Training set score: %f" % mlp.score(norm_X_train, Y_train))
+print("Test set score: %f" % mlp.score(norm_X_test, Y_test))
